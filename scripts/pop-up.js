e8bb57e8e94f12bb70b454738184b7a3d2253149
Jesús Moreno <jmm12392@gmail.com>
2023-06-30 09:36:08 -0400

    create pop up js file

---
+++ scripts/pop-up.js
@@ -0,0 +1,55 @@
+// const works = [
+//   {
+//     id: '1',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique pariatur iste suscipit nesciunt aut obcaecati numquam?',
+//     Image: './src/Mask Group img.svg',
+//     technologies: ['HTML', 'Bootstrap', 'ruby'],
+//     liveDemo: 'https://xecyc.github.io/My-Portfolio-Project/',
+//     projectURL: 'https://github.com/Xecyc/My-Portfolio-Project',
+//   },
+  
+//   {
+//     id: '2',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique pariatur iste suscipit nesciunt aut obcaecati numquam?',
+//     Image: './src/Mask Group img.svg',
+//     technologies: ['HTML', 'Bootstrap', 'ruby'],
+//     liveDemo: 'https://xecyc.github.io/My-Portfolio-Project/',
+//     projectURL: 'https://github.com/Xecyc/My-Portfolio-Project',
+//   },
+
+//   {
+//     id: '3',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique pariatur iste suscipit nesciunt aut obcaecati numquam?',
+//     Image: './src/Mask Group img.svg',
+//     technologies: ['HTML', 'Bootstrap', 'ruby'],
+//     liveDemo: 'https://xecyc.github.io/My-Portfolio-Project/',
+//     projectURL: 'https://github.com/Xecyc/My-Portfolio-Project',
+//   },
+
+//   {
+//     id: '4',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique pariatur iste suscipit nesciunt aut obcaecati numquam?',
+//     Image: './src/Mask Group img.svg',
+//     technologies: ['HTML', 'Bootstrap', 'ruby'],
+//     liveDemo: 'https://xecyc.github.io/My-Portfolio-Project/',
+//     projectURL: 'https://github.com/Xecyc/My-Portfolio-Project',
+//   },
+
+//   {
+//     id: '5',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique pariatur iste suscipit nesciunt aut obcaecati numquam?',
+//     Image: './src/Mask Group img.svg',
+//     technologies: ['HTML', 'Bootstrap', 'ruby'],
+//     liveDemo: 'https://xecyc.github.io/My-Portfolio-Project/',
+//     projectURL: 'https://github.com/Xecyc/My-Portfolio-Project',
+//   },
+
+//   {
+//     id: '6',
+//     description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique pariatur iste suscipit nesciunt aut obcaecati numquam?',
+//     Image: './src/Mask Group img.svg',
+//     technologies: ['HTML', 'Bootstrap', 'ruby'],
+//     liveDemo: 'https://xecyc.github.io/My-Portfolio-Project/',
+//     projectURL: 'https://github.com/Xecyc/My-Portfolio-Project',
+//   },
+// ]
